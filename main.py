4798a595aebcc958b89829b4d122caa940a8a496
Gustavo Meatto <46137871+WaterDeveloper@users.noreply.github.com>
2021-02-23 11:39:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # CONFIG
-token = "TOKEN" # Just google how to get discord token.
-prefix = "~"
+token = "TOKEN" # Pesquise no google como achar isso.
+prefix = "!"
 
 
-# Import the needed libs.
+
 import asyncio
 from discord.ext import commands
 import discord
@@ -15,11 +15,11 @@ print("Loading..")
 words = ['esse jogo é legal', 'nice client', 'alguem quer duo plat', '.', 'alguem pode me ajuda?', 'como assim', 'entendi', 'oi', 'obrigado', 'desculpa', 'sério?', 'pq eu n consigo entrar no lol?', 'ele morreu e falou: Eh o grongos', 'alguem pode me doar rp?', 'n consigo falar no chat do lol', 'pq eu ganhei pouco pdl?', 'não entendi', 'saquei', 'kkkkkkkkk', 'boa man', 'diga não aos discord modificados', 'impossivel jogar lol vei', 'pq n respondem?', 'sou eu', 'oq é gankar?', 'dsclp sou novo', 'kkkkkk (rindo de nervoser)', 'escutem K/DA galera', 'sério cara?', 'pq isso man? calma drill!', 'Oieee', 'Oi, meu nome é pedro sou ferro e éh isso', 'impossivel vey', 'hj n', 'yone mt op lek', 'eh de f3d3er', 'queue de ferro = pouco pdl', 'tá f0d4', 'algm duo?', 'fleex agr go', '#GOpaiN', '#GOFLA', '#GOKabuM!', 'é o grongos', 'só a cabecinha', 'só darius abuser nisso, credo.', 'darius tá mt forte cara', 'tinowns pernas de camille', '4lan mão fofa', 'só o básico hehe', 'cheio de tóxico no lol credo', 'jogar lol é frustante lek', 'bruh ;-;', 'carroça', 'tá tendo uns drop insano ultimamente hein', 'muito massa o teemo', 'é dahora de jogar de irelia man', 'cabei de feedar de yasuo kkk', 'aphelios é legal?']
 
 #   Count bot config
-counting_channel = 718190688356794418 #	Channel for auto counter
+counting_channel = 718190688356794418 #	ID do canal
 enabled = True
 last_number = 0
 
-# Declare the bot, pass it a prefix and let it know to only listen to itself.
+
 
 bot = commands.Bot(command_prefix=prefix, self_bot=True)
 bot.remove_command("help")
@@ -77,4 +77,4 @@ async def on_message(message):
 
 
 
-bot.run(token, bot=False)  # Starts the bot by passing it a token and telling it it isn't really a bot. 
+bot.run(token, bot=False)  
